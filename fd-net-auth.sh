0a8daa6f446aadd0ef83c5097b9cf99df55da372
Runxi Yu <runxiyu@andrewyu.org>
2024-04-21 09:15:51 +0800

    Fudan University wired internet authentication script

---
+++ fd-net-auth.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+URL="http://10.250.3.66/include/auth_action.php"
+read -p "Username: " username
+read -p "Password: " -s password
+echo
+ip="$(ip -f inet addr show eno0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
+mac="$(ip -f link addr show eno0 | sed -En -e 's/.*link\/ether ([0-9a-f:]+).*/\1/p')"
+curl $URL --insecure --data "action=login&username=$username&password=$password&ac_id=1&user_ip=$ip&nas_ip=127.0.0.1&user_mac=$mac&save_me=1&ajax=1"
