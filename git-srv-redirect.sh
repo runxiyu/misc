82ddaa3993bb488421d57cec5ed00cce0d30ed51
Runxi Yu <s22537@stu.ykpaoschool.cn>
2024-04-27 22:15:54 +0800

    Comments on git-srv-redirect.sh

--- git-srv-redirect.sh
+++ git-srv-redirect.sh
@@ -1,5 +1,23 @@
 #!/bin/bash                            
+#
+# Written by Runxi Yu <me@runxiyu.org>
+# Public domain / CC0
+# 
+# Usage:
+# 1. Prepend command="/path/to/this/script" to a key line in
+#    authorized_keys on the server
+# 2. Generate or otherwise have a git-specific SSH key on the client
+# 3. Configure the client's .ssh/config such as:
+#    host rxgit
+#        user me
+#        hostname runxiyu.org
+#        port 22
+#        identityfile ~/.ssh/id_ed25519_git
+# 4. Now, git clone rxgit:relative_path will use /srv/git/relative_path
+#    instead, while absolute paths are unaffected.
+#
+
 declare -a "s=($SSH_ORIGINAL_COMMAND)" 
-p="${s[1]}"                            
-[ "$p" = "${p#/}" ] && p=/srv/git/"$p" 
-exec git-shell -c "${s[0]} ${p@Q}"     
+p="${s[1]}"
+[ "$p" = "${p#/}" ] && p=/srv/git/"$p"
+exec git-shell -c "${s[0]} ${p@Q}"
