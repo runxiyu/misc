13eb827afd2af28c782ae70fba888bdd15dcc28e
Runxi Yu <me@runxiyu.org>
2024-05-25 19:37:18 +0800

    ipld.c: Add a stupid C program that echos IP addresses

---
+++ ipld.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include <netinet/in.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int socket_desc, new_socket, c;
+	struct sockaddr_in server, client;
+	// char *message;
+
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_desc == -1) {
+		return 1;
+	}
+
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(1234);
+
+	if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		return 2;
+	}
+
+	listen(socket_desc, 3);
+
+	for (;;) {
+		c = sizeof(struct sockaddr_in);
+		new_socket =
+		    accept(socket_desc, (struct sockaddr *)&client,
+			   (socklen_t *) & c);
+		if (new_socket < 0) {
+			return 3;
+		}
+
+		// int client_port = ntohs(client.sin_port);
+		char *client_ip = inet_ntoa(client.sin_addr);
+		for (int i = 0; i < 100; ++i) {
+			if (client_ip[i] == 0)
+				break;
+			printf("%c", client_ip[i]);
+		}
+		printf("\n");
+		write(new_socket, client_ip, strlen(client_ip));
+		write(new_socket, "\n", 1);
+	}
+	return 0;
+}
