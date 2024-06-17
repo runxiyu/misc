5325d86d26eaa5b24b87b165e2e0743cb1d9ff6a
Runxi Yu <me@runxiyu.org>
2024-06-17 08:00:00 +0800

    Add repo.sh (git.sr.ht and github.com)

---
+++ repo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "$2" ] || [ ! -z "$3" ]
+then
+	printf 'usage: %s repo-name repo-description\n' "$0" >&2
+	exit 1
+fi
+
+name="$(basename "$1" .git)"
+
+hut git create "$name" -v public -d "$2"
+gh repo create "$name" --disable-issues --disable-wiki -d "$2" --public -h "https://git.sr.ht/~runxiyu/$1"
