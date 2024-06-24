1818cc6c11793e9c80d3a82faeed5c0acc1e6f92
Runxi Yu <me@runxiyu.org>
2024-06-24 08:00:00 +0800

    a

--- go-static-build.sh
+++ go-static-build.sh
@@ -1,1 +1,2 @@
-CGO_ENABLED=0 go build -ldflags '-extldflags "-f no-PIC -static"' -tags 'osusergo netgo static_build'
+#!/bin/sh
+CGO_ENABLED=0 go build -ldflags '-extldflags "-f no-PIC -static"' -tags 'osusergo netgo static_build' "$@"
