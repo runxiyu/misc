14a96fc1ec78f0b3e88a1841d58ece8c47a1d474
Runxi Yu <me@runxiyu.org>
2024-08-02 08:00:00 +0800

    repo.sh: Codeberg fix

--- repo.sh
+++ repo.sh
@@ -10,4 +10,4 @@ name="$(basename "$1" .git)"
 
 hut git create "$name" -v public -d "$2"
 gh repo create "$name" --disable-issues --disable-wiki -d "$2" --public -h "https://git.sr.ht/~runxiyu/$1"
-berg --non-interactive repo create -n "$name" -d "$2" -p public # --default-branch master
+berg --non-interactive repo create -n "$name" -d "$2" -p public --default-branch master
