7585fcedac509ff59e060263e7461c2509cc1a02
Runxi Yu <me@runxiyu.org>
2024-08-18 08:00:00 +0800

    blog and tty large changes

--- tty-large.sh
+++ tty-large.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-setfont /usr/lib/kbd/consolefonts/latarcyrheb-sun32.psfu.gz
+# setfont /usr/lib/kbd/consolefonts/latarcyrheb-sun32.psfu.gz
+setfont /usr/lib/kbd/consolefonts/ter-v32b.psf.gz
